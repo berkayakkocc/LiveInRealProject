1d2b9acc989e4e174647fdd53d23226a71dee05f
berkayakkocc <77438994+berkayakkocc@users.noreply.github.com>
2022-07-16 02:01:57 +0300

    Neler oldu Neler ?!!

---
+++ ProjectOfNew/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using Business.Concrete;
+using DataAccess.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LiveInRealProject.Controllers
+{
+    public class ProductController : Controller
+    {
+        ProductManager productManager = new ProductManager(new EfProductDal());
+        public IActionResult Index()
+        {
+           var values= productManager.GetList();
+            return View(values);
+        }
+    }
+}
