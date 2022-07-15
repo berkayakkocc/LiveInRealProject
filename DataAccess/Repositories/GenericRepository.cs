7d4b874af14fbc1671e5b32516cb7c23b92bc07a
berkayakkocc <77438994+berkayakkocc@users.noreply.github.com>
2022-07-15 04:21:18 +0300

    Add project files.

---
+++ DataAccess/Repositories/GenericRepository.cs
@@ -0,0 +1,46 @@
+﻿using DataAccess.Abstract;
+using DataAccess.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repositories
+{
+    public class GenericRepository<T> : IGenericDal<T> where T : class
+    {
+
+        public void Add(T entity)
+        {
+            using Context db = new Context();
+            db.Add(entity);
+            db.SaveChanges();
+        }
+
+        public void Delete(T entity)
+        {
+            using Context db = new Context();
+            db.Remove(entity);
+            db.SaveChanges();
+        }
+        public void Update(T entity)
+        {
+            using Context db = new Context();
+            db.Update(entity);
+            db.SaveChanges();        }
+        public T GetById(int id)
+        {
+            using Context db = new Context();
+            return db.Set<T>().Find(id);
+        }
+
+        public List<T> GetList()
+        {
+            using Context db = new Context();
+            return db.Set<T>().ToList();
+        }
+
+       
+    }
+}
